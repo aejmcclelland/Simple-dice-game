85395e25e814f075fea3686842b8d63a7823443d
aejmcclelland <aejmcclelland@googlemail.com>
2023-11-28 22:18:16 +0000

    added Hello styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
-
+import './Hello.css'
 import Hello from './Hello';
 
 function App() {
   const helloComponents = Array(3).fill(<Hello />);
 
   return (
-    <div>
+    <div className="Hello">
       {helloComponents}
     </div>
   );
